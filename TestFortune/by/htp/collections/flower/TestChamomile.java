be4ba8797c278ab033f7b9ef89410d907c7fbdcc
MaryAparovich <36172265+MaryAparovich@users.noreply.github.com>
2018-03-23 15:01:14 +0300

    upd method testPetalCount

--- TestFortune/by/htp/collections/flower/TestChamomile.java
+++ TestFortune/by/htp/collections/flower/TestChamomile.java
@@ -4,34 +4,30 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.LinkedList;
 import java.util.List;
-
+import org.junit.Before;
 import org.junit.Test;
-
 import by.htp.collections.flower.Chamomile;
 import by.htp.collections.flower.Flower;
 import by.htp.collections.flower.Petal;
+import by.htp.collections.gardener.Gardener;
 
 public class TestChamomile {
 	
+	private List<Petal> petals;
+	private Gardener gardener;
+	private Flower flower;
+	
 	@Test
 	public void testIfNullPetals() {
-		List<Petal> petals = new LinkedList<>();
-		Flower flower = new Chamomile("Ромашка", petals);
+		petals = new LinkedList<>();
+		Flower flower = new Chamomile("Р РѕРјР°С€РєР°", petals);
 		assertEquals(0, flower.getPetals().size());
 	}
 
 	@Test
 	public void testPetalCount() {
-		int petalCount = 5;
-		List<Petal> listPetal = new LinkedList<>();
-		
-		for (int i = 0; i < petalCount; i++) {
-			Petal petal = new Petal();
-			listPetal.add(petal);
-		}
-		
-		Flower flower = new Chamomile("Ромашка", listPetal);
-		
-		assertEquals(listPetal, flower.getPetals());
+		gardener = new Gardener();
+		flower = gardener.getChamomile("Р РѕРјР°С€РєР°", 5);
+		assertEquals(5, flower.getPetals().size());
 	}
 }
